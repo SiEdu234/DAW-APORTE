6399f187323bde4f3323106866a4ed70c87a2101
Rafael <simedua12@gmail.com>
2026-05-12 08:08:56 -0500

    coso

--- daw/settings.py
+++ daw/settings.py
@@ -77,13 +77,9 @@ WSGI_APPLICATION = 'daw.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "DWA",
-        "USER": "postgres",
-        "PASSWORD": "admin",
-        "HOST": "127.0.0.1",
-        "PORT": "5432",
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
